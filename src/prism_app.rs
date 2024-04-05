7b5b1f86abf832fefeb75970aa3a3be632666c0d
Srinu427 <srinivas.adigopula@gmail.com>
2024-04-05 13:00:34 +0530

    fix event run lambda fn

--- src/prism_app.rs
+++ src/prism_app.rs
@@ -116,13 +116,13 @@ impl PrismApp {
                         ..
                     } => {
                         self.renderer.set_camera(self.camera);
-                        self.renderer.draw()
-                            .map_err(|e| println!("renderer draw error: {}", e))?
+                        let _ = self.renderer.draw()
+                            .map_err(|e| println!("renderer draw error: {}", e));
                     }
                     Event::AboutToWait => {
                         self.renderer.set_camera(self.camera);
-                        self.renderer.draw()
-                            .map_err(|e| println!("renderer draw error: {}", e))?
+                        let _ = self.renderer.draw()
+                            .map_err(|e| println!("renderer draw error: {}", e));
                     }
                     _ => (),
                 }
